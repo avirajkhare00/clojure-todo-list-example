e54d5d9c2697ca38993d3002d2e2c49badb8cab2
Aviraj Khare <aviraj.khare@aux.go-jek.com>
2019-05-10 01:40:28 +0530

    first commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject todo-list "0.1.0-SNAPSHOT"
+  :description "A todo list server side webapp using Ring and Compojure"
+  :url "https://github.com/avirajkhare00/clojure-todo-list-example"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns todo-list.core})
